bef893adf94c150ed59ff5fb725a2560180a9b64
Ayush Mukherjee <111524872+Ayush5421@users.noreply.github.com>
2024-10-02 22:05:46 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle');
-
 navToggle.addEventListener('click', () => {
   navMenu.classList.toggle('show-menu');
 });
